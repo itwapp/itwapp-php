38a73990d79cfd533bcbd8e98c47ef9004bfdfaf
Jerome Heissler <heissler.jerome@gmail.com>
2014-12-19 16:47:48 +0100

    update coveralls config

--- lib/Itwapp.php
+++ lib/Itwapp.php
@@ -1,21 +1,21 @@
 <?php
 
 // Itwapp singleton
-require(dirname(__FILE__) . '/Itwapp/Itwapp.php');
+require_once(dirname(__FILE__) . '/Itwapp/Itwapp.php');
 
 // Errors
-require(dirname(__FILE__) . '/Itwapp/exception/InvalidRequestError.php');
-require(dirname(__FILE__) . '/Itwapp/exception/ResourceNotFoundException.php');
-require(dirname(__FILE__) . '/Itwapp/exception/ServiceException.php');
-require(dirname(__FILE__) . '/Itwapp/exception/UnauthorizedException.php');
+require_once(dirname(__FILE__) . '/Itwapp/exception/InvalidRequestError.php');
+require_once(dirname(__FILE__) . '/Itwapp/exception/ResourceNotFoundException.php');
+require_once(dirname(__FILE__) . '/Itwapp/exception/ServiceException.php');
+require_once(dirname(__FILE__) . '/Itwapp/exception/UnauthorizedException.php');
 
 // Utilities
-require(dirname(__FILE__) . '/Itwapp/rest/ApiRequest.php');
-require(dirname(__FILE__) . '/Itwapp/rest/Sign.php');
+require_once(dirname(__FILE__) . '/Itwapp/rest/ApiRequest.php');
+require_once(dirname(__FILE__) . '/Itwapp/rest/Sign.php');
 
 // Itwapp API Resources
-require(dirname(__FILE__) . '/Itwapp/object/Applicant.php');
-require(dirname(__FILE__) . '/Itwapp/object/Interview.php');
-require(dirname(__FILE__) . '/Itwapp/object/Question.php');
-require(dirname(__FILE__) . '/Itwapp/object/Response.php');
+require_once(dirname(__FILE__) . '/Itwapp/object/Applicant.php');
+require_once(dirname(__FILE__) . '/Itwapp/object/Interview.php');
+require_once(dirname(__FILE__) . '/Itwapp/object/Question.php');
+require_once(dirname(__FILE__) . '/Itwapp/object/Response.php');
 
